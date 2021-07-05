39a0b62cecd85df0f5c45e9508bd93eda7623f25
Petikov Vladimir <developer-krox@yandex.ru>
2021-07-05 18:32:17 +0300

    Создание портфолио - 01

---
+++ sites/portfolio.loc/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+<!--          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">-->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Мое портфолио</title>
+    <link rel="stylesheet" href="./css/krox-pf.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;
+                                                                                      900&display=swap">
+</head>
+<body>
+    <div class="container">
+        <div class="business-card">
+            <div class="info">
+                <span class="profession">Веб-разработчик</span>
+                <span class="name">Владимир Петиков</span>
+                <p>Разрабатываю сайты для малого и среднего бизнеса.
+                    У меня можно заказать дизайн и верстку небольшого сайта или интернет-магазина</p>
+                <a href="#">Бесплатная&nbsp консультация</a>
+            </div>
+            <div class="img-card"></div>
+        </div>
+        <div class="project">
+<!--            <div class="project-card">-->
+<!--                <a href="https://uni.kroxdev.ru">Новостной портал Universal</a>-->
+<!--                <p>Universal — это онлайн издание, в котором мы публикуем полезные-->
+<!--                    материалы о веб разработке: статьи, новости, видеоуроки, подборки, ссылки и курсы.</p>-->
+<!--            </div>-->
+
+            <div class="project-card">
+                <a href="https://uni.kroxdev.ru">
+                <div class="project-card-info">
+                    <span>Новостной портал Universal</span>
+                    <p>Universal — это онлайн издание, в котором мы публикуем полезные
+                        материалы о веб разработке: статьи, новости, видеоуроки, подборки, ссылки и курсы.</p>
+                </div>
+            </a>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
